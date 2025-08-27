21b98c5ee7446799724dea752f1772355bffcd4a
Christopher Vigliotti <christopher@christophervigliotti.com>
2025-08-27 15:43:17 -0400

    Aug 27 - rendering techniques

--- rendering-techniques/src/App.jsx
+++ rendering-techniques/src/App.jsx
@@ -2,34 +2,86 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import PackingList from './PackingList.jsx'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App(){
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <PackingList />
+    </main>
   )
 }
+
+/*
+THE LESSON 
+
+// an example 
+// this is pretty cool...
+function App() {
+  const animals = ["Lion", "Cow", "Snake", "Lizard","Chupacabra","Bigfoot","Nessie","Snallygaster"];
+  const animalsList = animals.map((animal) => <li key={animal}>{animal}</li>)
+  return (
+    <div>
+      <h1>Animals: </h1>
+      <ul 
+        style=
+        {{
+          textAlign: 'left',
+          width: '150px'
+        }}
+      >
+        {animalsList}
+      </ul>
+    </div>
+  );
+}
+
+// ...but this is way cooler...
+// rendering a list of components
+function ListItem(props) {
+  return <li>{props.animal}</li>
+}
+
+function List(props) {
+
+  if (!props.animals) {
+    return <div>Loading...</div>;
+  }
+
+  if (props.animals.length === 0) {
+    return <div>There are no animals!  Sad!</div>;
+  }  
+
+  return (
+    <ul
+        style= 
+        {{
+          textAlign: 'left',
+          width: '150px'
+        }}    
+    >
+      {props.animals.map((animal) => {
+        // return <ListItem key={animal} animal={animal} />;
+        // return animal.startsWith("L") ? <li key={animal}>{animal}</li> : null;
+
+        return animal.startsWith("L") && <li key={animal}>{animal}</li>;
+
+      })}
+    </ul>
+  );
+}
+function App() {
+  // const animals = ["Lyger", "Cow", "Snake", "Lizard","Chupacabra","Bigfoot","Nessie","Snallygaster","Larry"];
+  const animals = [];
+
+  // <List /> when you don't specify animals= prop, will display "Loading..."
+  return (
+    <div>
+      <h1>Animals!</h1>
+      <List animals={animals} />
+    </div>
+  );
+}
+*/
 
 export default App
