2d30b927c659e55527d388e7c5eb2703f263c90e
Christopher Vigliotti <christopher@christophervigliotti.com>
2025-08-28 09:53:02 -0400

    tidying up

--- rendering-techniques/src/PackingList.jsx
+++ rendering-techniques/src/PackingList.jsx
@@ -1,25 +1,53 @@
 function Item({ name, isPacked }) {
-  return <li className="item">{name}</li>;
+    /*
+    if (isPacked) {
+        // return <li className="item">{name} ✅</li>;
+        // return null;
+        <li className="item">
+        {isPacked ? name + ' ✅' : name}
+        </li>  
+    }
+    return <li className="item">{name} 😱</li>;
+
+    return(
+        <li className="item">
+            {isPacked ? name + ' ✅' : name + ' 😱'}
+        </li>  
+    );
+    */
+  return (
+    <li className="item">
+      {isPacked ? (
+        <del>
+          {name + ' ✅'}
+        </del>
+      ) : (
+        name + ' 😱😱😱'
+      )}
+    </li>
+  );
 }
 
 export default function PackingList() {
   return (
-    <section>
-      <h1>Sally Ride's Packing List</h1>
-      <ul>
-        <Item 
-          isPacked={true} 
-          name="Space suit" 
-        />
-        <Item 
-          isPacked={true} 
-          name="Helmet with a golden leaf" 
-        />
-        <Item 
-          isPacked={false} 
-          name="Photo of Tam" 
-        />
-      </ul>
-    </section>
+    <>
+        <section>
+        <h1>Sally Ride's Packing List</h1>
+        <ul>
+            <Item 
+            isPacked={true} 
+            name="Space suit" 
+            />
+            <Item 
+            isPacked={true} 
+            name="Helmet with a golden leaf" 
+            />
+            <Item 
+            isPacked={false} 
+            name="Photo of Tam" 
+            />
+        </ul>
+        </section>
+    </>
   );
 }
